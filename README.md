f813d70a1326085db2a7af99d8582c3ba24eff6b
Tiago Hillebrandt <tiago@tiagohillebrandt.com>
2020-04-15 08:51:24 -0300

    Parse link header implementation.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# php-parse-link-header
-Parse the HTTP Link header in PHP
+# tiagohillebrandt/php-parse-link-header
+Parse the HTTP Link header and return the values as an array.
+
+## Installation with Composer
+
+```bash
+$ composer require tiagohillebrandt/php-parse-link-header
+```
+
+## Usage
+
+```php
+$headers = [
+    'Link' => '<https://api.github.com/organizations/xyz/repos?page=2>; rel="next", <https://api.github.com/organizations/xyz/repos?page=4>; rel="last"',
+];
+
+$links = ( new ParseLinkHeader( $headers['Link'] ) )->toArray();
+
+print_r( $links );
+```
+
+The above example will output:
+
+```
+Array
+(
+    [next] => Array
+        (
+            [link] => https://api.github.com/organizations/xyz/repos?page=2
+            [page] => 2
+        )
+
+    [last] => Array
+        (
+            [link] => https://api.github.com/organizations/xyz/repos?page=4
+            [page] => 4
+        )
+
+)
+```
---
+++ src/ParseLinkHeader.php
@@ -0,0 +1,80 @@
+<?php
+namespace TiagoHillebrandt;
+
+/**
+ * Parse Link Header.
+ *
+ * @package TiagoHillebrandt
+ * @author Tiago Hillebrandt <tiago@tiagohillebrandt.com>
+ */
+class ParseLinkHeader
+{
+	/**
+	 * The 'Link' header original value.
+	 *
+	 * @var string|null
+	 */
+	protected $linkHeader = null;
+
+	/**
+	 * ParseLinkHeader constructor.
+	 *
+	 * @param string $linkHeader The 'Link' header original value.
+	 */
+	public function __construct($linkHeader)
+	{
+		$this->linkHeader = $linkHeader;
+	}
+
+	/**
+	 * Parses the link header and returns the values as an array.
+	 *
+	 * @return array
+	 */
+	public function toArray()
+	{
+		if (!is_string($this->linkHeader))
+		{
+			error_log('The argument needs to be a valid string');
+		}
+
+		if (false === strpos($this->linkHeader, ',')) {
+			$links[] = $this->linkHeader;
+		} else {
+			$links = explode(',', $this->linkHeader);
+		}
+
+		if (!is_array($links) || empty($links))
+		{
+			error_log('The link header value is empty or is not valid');
+		}
+
+		$values = [];
+
+		foreach (explode(',', $this->linkHeader) as $header)
+		{
+			$values = array_merge($values, $this->extractData($header));
+		}
+
+		return $values;
+	}
+
+	/**
+	 * Extracts header data.
+	 *
+	 * @param string $linkHeader The 'Link' header value.
+	 *
+	 * @return array
+	 */
+	public function extractData($linkHeader)
+	{
+		preg_match('/<(.*?(?:page=(\d))?)>.*rel="(.*)"/', $linkHeader, $matches, PREG_UNMATCHED_AS_NULL);
+
+		return [
+			$matches[3] => [
+				'link' => $matches[1],
+				'page' => $matches[2],
+			],
+		];
+	}
+}
